fa8a3d04413f378b014ad9ee3ceafbd84d761d1d
Jonathan Azoff <jon@azoffdesign.com>
2012-02-09 02:53:39 -0800

    basic REPL working

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,61 @@
-(function(global, bpm){
+(function(global, bpm, undef){
     
-    var console;
-    
-    function startConsole() {
-        $(function () {
-            var jqconsole = $('#console').jqconsole('Hi\n', '>>>');
-            var startPrompt = function () {
-                // Start the prompt with history enabled.
-                jqconsole.Prompt(true, function (input) {
-                    // Output input with the class jqconsole-output.
-                    jqconsole.Write(input + '\n', 'jqconsole-output');
-                    // Restart the prompt.
-                    startPrompt();
-                });
-            };
-            startPrompt();
+    function repl(console) {
+        console.Prompt(true, function(input) {
+            try { var output;
+                if (input !== undef && input.length) {
+                    output = eval(input) || '';
+                } else {
+                    output = '';
+                }                
+                logger.info(output);
+            } catch(e) {
+                logger.error(e.toString());
+            } finally {
+                repl(console);
+            }
         });
     }
+    
+    function logger(console, container) {
+        return {
+            info: function(msg) {
+                if (msg.length) {
+                    console.Write(msg + '\n', 'jqconsole-output');
+                }
+            },
+            error: function(msg) {
+                if (msg.length) {
+                    console.Write(msg + '\n', 'jqconsole-error');
+                }
+            },
+            clear: global.clear = function() {
+                container.find('.jqconsole-prompt').prevAll(':not(.jqconsole-header)').remove();
+            },
+            prefill: function(msg) {
+                console.SetPromptText(msg);
+            }
+        };
+    }
+    
+    function main() {
+        var selector = '#console',
+        msg = 'Take bpm for a test drive! Need help? Run bpm.usage().\n',
+        prompt = 'bpm$ ', console, container;
+        $(function() {
+            container = $(selector);
+            console = container.jqconsole(msg, prompt);
+            bpm.logger = logger = logger(console, container);
+            repl(console);
+            logger.prefill('bpm.install("dojo");');            
+        });
+    }    
     
     bpm.define('jqconsole', {
         versions: ['2.7'],
         cdn: 'assets/jq-console-${v}.min.js'
     });
     
-    bpm.install(['jquery', 'jqconsole'], startConsole);
+    bpm.install(['jquery', 'jqconsole'], main);
     
 })(window, window.bpm);
